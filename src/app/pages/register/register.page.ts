4693660ed008c03b275290586fc81424d7d733a6
Auraria <137568419+Franky1764@users.noreply.github.com>
2025-05-29 23:24:09 -0400

    Feat (auth): implement user registration with Firabase Auth and Firestore

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,3 +1,4 @@
+import { ToastController } from '@ionic/angular';
 // src/pages/register/register.page.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -20,6 +21,7 @@ export class RegisterPage {
   constructor(
     private fb: FormBuilder, 
     private authService: AuthService, // Asegúrate de que AuthService esté importado correctamente
+    private toastController: ToastController,
     private router: Router) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
@@ -31,14 +33,51 @@ export class RegisterPage {
 
 async onSubmit() {
   this.submitted = true;
-  if (this.registerForm.valid && this.passwordsMatch()) {
-    const { name, email, password } = this.registerForm.value;
-    try {
-      await this.authService.register(name, email, password);
-      this.router.navigateByUrl('/dashboard');
-    } catch (error) {
-      console.error('Error al registrar usuario:', error);
-    }
+
+  if (this.registerForm.invalid || !this.passwordsMatch()) {
+    return;
+  }
+
+  const { name, email, password } = this.registerForm.value;
+
+  try {
+    await this.authService.register(name, email, password);
+
+    const toast = await this.toastController.create({
+      message: 'Usuario registrado exitosamente. Inicia sesión.',
+      duration: 4500,
+      color: 'success',
+      position: 'middle',
+    });
+    await toast.present();  
+
+    this.registerForm.reset(); // Limpia el formulario
+    this.submitted = false;
+
+    this.router.navigate(['/login']); // Redirige al login
+
+    
+
+  } catch (error:any) {
+    let message = 'Error al registrar usuario.';
+    
+  if (error.code === 'auth/weak-password') {
+    message = 'La contraseña debe tener al menos 6 caracteres.';
+  } else if (error.code === 'auth/email-already-in-use') {
+    message = 'Este correo ya está registrado. Intenta iniciar sesión.';
+  } else if (error.code === 'auth/invalid-email') {
+    message = 'El correo electrónico no es válido.';
+  }
+    
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 4500,
+      color: 'danger',
+      position: 'middle',
+    });
+    await toast.present();
+    this.registerForm.reset(); // Limpia el formulario
+    this.submitted = false;
   }
 }
   passwordsMatch(): boolean {

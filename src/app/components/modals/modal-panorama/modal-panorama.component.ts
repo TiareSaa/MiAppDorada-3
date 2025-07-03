dd8a3aeaf574be71a54bf36dffa2b028ff8d2fd2
Auraria <137568419+Franky1764@users.noreply.github.com>
2025-07-03 19:04:53 -0400

    feat (modal panoramas) add map for activities

--- src/app/components/modals/modal-panorama/modal-panorama.component.ts
+++ src/app/components/modals/modal-panorama/modal-panorama.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalController, IonicModule} from '@ionic/angular';
+import { Panorama } from 'src/app/services/panorama.service';
 
 
 @Component({
@@ -21,5 +22,17 @@ export class ModalPanoramaComponent {
   inscribirse() {
     alert('¡Inscripción enviada con éxito!');
     this.modalCtrl.dismiss();
+  }
+  
+  verEnMapa() {
+    alert('Abriendo mapa para el evento: ' + this.evento.titulo);
+    this.modalCtrl.dismiss();
+    const ubicacion = this.evento.ubicacion;
+    if (ubicacion && ubicacion.latitude && ubicacion.longitude) {
+      const url = `https://www.google.com/maps/search/?api=1&query=${ubicacion.latitude},${ubicacion.longitude}`;
+      window.open(url, '_blank');
+    } else {
+      console.error('Ubicación no disponible para este evento.');
+    }
   }
 }

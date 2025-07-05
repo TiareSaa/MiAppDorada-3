36977200809d744a9936726d985db3695c09cbeb
Auraria <137568419+Franky1764@users.noreply.github.com>
2025-07-05 13:50:02 -0400

    feat (app.component) add menu for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { IonApp, IonRouterOutlet, IonHeader, IonMenu, IonToolbar, IonTitle, 
+          IonContent, IonList,IonItem, IonMenuToggle, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonMenu, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonMenuToggle, IonLabel],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private auth: Auth, private router: Router) {}
+  async logout() {
+    await this.auth.signOut();
+    this.router.navigate(['/login']);
+  }
+
+  navigateTo(route: string) {
+  this.router.navigate([route]);
+}
+
 }

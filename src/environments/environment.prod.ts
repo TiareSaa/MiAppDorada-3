32402302459fdf02a302996971a6744bd866e96e
TiareSaa <164646430+TiareSaa@users.noreply.github.com>
2025-07-08 14:25:53 -0400

    Agrega archivos de entorno para build en Appflow

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyARMZ8GiL6YTCOSkzBff5ZhE0LoOXvGIwc",
+    authDomain: "miappdorada.firebaseapp.com",
+    projectId: "miappdorada",
+    storageBucket: "miappdorada.appspot.com",
+    messagingSenderId: "620597484097",
+    appId: "1:620597484097:web:f65b2f0a39a49d5e6c84c0",
+    measurementId: "G-TR2KW47VND"
+  }
+};
